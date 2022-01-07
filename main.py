f5482e569c79df47cfdf5ceb64883c78098ebce8
mkofler96 <rasta.kof@gmail.com>
2022-01-07 19:13:49 +0100

    small changes

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import csv
-import requests
 import pandas as pd
 import matplotlib.pyplot as plt
+import streamlit as st
+#import seaborn as sns
 
 CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSk9VyduRn6RXuibvKtgoGcRjrn0hGjm5iuGz5nzc8T4Jg68im8ck7hYr8nsnEeBDDJlgKVz0bSjPrV/pub?output=csv'
+#st.set_page_config(layout="wide")
+
+st.markdown("<h1 style='text-align: center;'>Climbing Stats</h1>", unsafe_allow_html=True)
+st.markdown("<a href='https://docs.google.com/spreadsheets/d/1PJJMCrKVytjHbhaXi10CH5E-LDQUj1R4Xw3UDUvMvg4/edit#gid=2044315872' target='_blank'>edit google sheet</a>", unsafe_allow_html=True)
+st.markdown("""---""")
 
 df = pd.read_csv(CSV_URL)
 df['Datum'] = pd.to_datetime(df['Datum'], format='%d.%m.%Y')
@@ -11,6 +16,7 @@ df['Gewicht'] = df['Gewicht'].str[:-3].str.replace(',', '.').astype(float)
 df['Dauer'] = df['Dauer'].str.replace(',', '.').astype(float)
 print(df)
 
-plt.plot(df["Datum"], df["Dauer"], '-o', linewidth=2)
+f, ax = plt.subplots()
+plt.bar(df["Datum"], df["Dauer"])
 
-plt.show()
+st.pyplot(f)
