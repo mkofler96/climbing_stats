7902a23371a0288479b48534493c9b650bb81800
mkofler96 <rasta.kof@gmail.com>
2022-01-07 22:37:01 +0100

    aesthetics

---
+++ helper.py
@@ -0,0 +1,26 @@
+import base64
+import streamlit as st
+
+def set_bg_hack(main_bg):
+    '''
+    A function to unpack an image from root folder and set as bg.
+    The bg will be static and won't take resolution of device into account.
+    Returns
+    -------
+    The background.
+    '''
+    # set bg name
+    main_bg_ext = "jpg"
+
+    st.markdown(
+        f"""
+         <style>
+         .reportview-container {{
+             background-image: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+             background-size: cover;
+             background-attachment: scroll;
+         }}
+         </style>
+         """,
+        unsafe_allow_html=True
+    )
--- main.py
+++ main.py
@@ -2,16 +2,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 import datetime
+from helper import set_bg_hack
+import base64
 #import seaborn as sns
 
 CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSk9VyduRn6RXuibvKtgoGcRjrn0hGjm5iuGz5nzc8T4Jg68im8ck7hYr8nsnEeBDDJlgKVz0bSjPrV/pub?output=csv'
-#st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 
-st.markdown("<h1 style='text-align: center;'>Climbing Stats</h1>", unsafe_allow_html=True)
-st.markdown("<a href='https://docs.google.com/spreadsheets/d/1PJJMCrKVytjHbhaXi10CH5E-LDQUj1R4Xw3UDUvMvg4/edit#gid=2044315872' target='_blank'>edit google sheet</a>", unsafe_allow_html=True)
-st.markdown("""---""")
+c_h1, c_h2 = st.columns([1.5, 3])
+c_h2.markdown("<h1 style='text-align: center'>Climbing Stats <a href='https://docs.google.com/spreadsheets/d/1PJJMCrKVytjHbhaXi10CH5E-LDQUj1R4Xw3UDUvMvg4/edit#gid=2044315872' target='_blank'>↗</a></h1>", unsafe_allow_html=True)
+c_h1.markdown("""---""")
+c_h2.markdown("""---""")
+c_a1, c_a2, c_a3 = st.columns([1.5, 1.5, 1.5])
+image = "climb_1.jpg"
 
+set_bg_hack(image)
 df = pd.read_csv(CSV_URL)
+
 df['Datum'] = pd.to_datetime(df['Datum'], format='%d.%m.%Y')
 df['Gewicht'] = df['Gewicht'].str[:-3].str.replace(',', '.').astype(float)
 df['Dauer'] = df['Dauer'].str.replace(',', '.').astype(float)
@@ -20,8 +27,18 @@ df['Wochenschnitt'] = df['Dauer'].rolling(7).mean()
 print(df)
 
 f, ax = plt.subplots()
-plt.bar(df["Datum"], df["Dauer"], color="aqua", alpha=0.5)
+f.patch.set_facecolor('none')
+ax.set_facecolor('none')
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_visible(True)
+ax.spines['left'].set_visible(False)
+ax.tick_params(axis='x', colors='white')
+ax.tick_params(axis='y', colors='white')
+ax.grid(axis='y',zorder=0, color="white")
+
+plt.bar(df["Datum"], df["Dauer"], color="darkslategrey", alpha=1, zorder=3)
 plt.plot(df["Datum"], df["Wochenschnitt"])
-ax.set_xlim([datetime.date(2022, 1, 1), datetime.date(2022, 3, 1)])
+ax.set_xlim([datetime.date(2022, 1, 1), datetime.date.today()])
 f.autofmt_xdate()
-st.pyplot(f)
+c_a3.pyplot(f)
